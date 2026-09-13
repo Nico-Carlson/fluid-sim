ed48e64ab58afcb3aac66b85bfc08cbde834a7f0
Nico <carlson.nico@gmail.com>
2026-09-13 17:50:47 -0600

    Added basic canvas and mouse coordinate functionality

---
+++ playground.js
@@ -0,0 +1,30 @@
+class Playground{
+
+  // constructor
+  constructor(){
+    this.simulation = new Simulation();
+  }
+
+  // update method
+  update(dt){
+    this.simulation.update(dt);
+  }
+
+  // draw method
+  draw(){
+    this.simulation.draw();
+  }
+
+  // mouse method
+  onMouseMove(x,y){
+    console.log("Mouse moved to: " +x+" "+y);
+  }
+
+  onMouseDown(button){
+    console.log("Mouse button pressed: "+button);
+  }
+
+  onMouseUp(button){
+    console.log("Mouse button released: "+button); 
+  }
+}
